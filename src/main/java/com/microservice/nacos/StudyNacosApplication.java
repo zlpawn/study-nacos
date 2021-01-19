2590e487f28499abd5c28246bf783910642842cb
zl <lsj515325@163.com>
2021-01-20 00:04:57 +0800

    Initial commit

---
+++ src/main/java/com/microservice/nacos/StudyNacosApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyNacosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/nacos/StudyNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
